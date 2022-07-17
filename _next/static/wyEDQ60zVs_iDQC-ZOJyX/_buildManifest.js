a018dd38685c897fb17e3535a9d8ea18d457e187
lukew3 <lukew25073@gmail.com>
2022-07-17 17:38:52 +0000

    Deploying to gh-pages from @ lukew3/arweave-nextjs-example@eed0d735b1cc2a481e2e2a2167521e05132cb9ae 🚀

---
+++ _next/static/wyEDQ60zVs_iDQC-ZOJyX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-637d9b04caf76dc0.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
